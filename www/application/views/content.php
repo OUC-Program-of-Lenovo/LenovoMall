6cdde6eb0796048fd401db012bb36d806ea66717
lizlizlizlizliz <472750237@qq.com>
2019-01-05 14:44:32 +0800

    商品列表页动态显示+购物车交互整合

--- www/application/views/content.php
+++ www/application/views/content.php
@@ -6,7 +6,7 @@
 	<h1 class="cover-heading"><span class="hint--right" aria-label="Notice!">Notice</span></h1>
 	<div class="lead">
 		<ul class="rules">
-			<li class="goods">
+			<!-- <li class="goods">
 				<img src="/assets/img/goods01.jpg">
 				<h1>拯救者 Y7000 15.6英寸游戏笔记本 黑色</h1>
 				<p>￥5699.00</p>
@@ -71,21 +71,23 @@
 				<h1>拯救者 Y7000 15.6英寸游戏笔记本 黑色</h1>
 				<p>￥5699.00</p>
 			</li>
-			<span class="ui-loading-icon" style="display: none;"></span>
+			<span class="ui-loading-icon" style="display: none;"></span> -->
 		</ul>
 	</div>
-</div>
-<div class="dialog">
-	<div class="dialog_content">
-		<button id="dialog_close">×</button>
-		<img src="/assets/img/goods01.jpg">
-		<div class="dialog_text">
-			<h1>拯救者 Y7000 15.6英寸游戏笔记本 黑色</h1>
-			<p>￥5699.00</p>
-			<button id="sub_num" class="num">-</button><input type="text" id="num" value="1"><button id="add_num" class="num">＋</button>
-			<button id="add_cart">ADD</button>
+
+	<div class="dialog">
+		<div class="dialog_content">
+			<button id="dialog_close">×</button>
+			<img src="/assets/img/goods01.jpg">
+			<div class="dialog_text">
+				<h1>拯救者 Y7000 15.6英寸游戏笔记本 黑色</h1>
+				<p>￥5699.00</p>
+				<button id="sub_num" class="num">-</button><input type="text" id="num" value="1"><button id="add_num" class="num">＋</button>
+				<button id="add_cart">ADD</button>
+			</div>
+			<p class="description">拯救者 Y7000 15.6英寸游戏笔记本 黑色拯救者 Y7000 15.6英寸游戏笔记本 黑色拯救者 Y7000 15.6英寸游戏笔记本 黑色拯救者 Y7000 15.6英寸游戏笔记本 黑色拯救者 Y7000 15.6英寸游戏笔记本 黑色拯救者 Y7000 15.6英寸游戏笔记本 黑色</p>
 		</div>
-		<p>拯救者 Y7000 15.6英寸游戏笔记本 黑色拯救者 Y7000 15.6英寸游戏笔记本 黑色拯救者 Y7000 15.6英寸游戏笔记本 黑色拯救者 Y7000 15.6英寸游戏笔记本 黑色拯救者 Y7000 15.6英寸游戏笔记本 黑色拯救者 Y7000 15.6英寸游戏笔记本 黑色</p>
 	</div>
 </div>
+
 <script type="text/javascript" src="/assets/js/content.js"></script>
--- www/html/assets/js/content.js
+++ www/html/assets/js/content.js
@@ -1,46 +1,134 @@
-
-
-
-
-//数量加减
-$("#sub_num").click(function(){
-    var num = parseInt($("#num").val())-1;
-    if(num<0){
-        $("#num").val(0);
+/*居中显示函数*/
+function funShowDivCenter(div) {
+        var top = ($(window).height(0
+        ) - $(div).height()) / 2;
+        var left = ($(window).width() - $(div).width()) / 2;
+        var scrollTop = $(document).scrollTop();
+        var scrollLeft = $(document).scrollLeft();
+        $(div).css({ display:'block',position: 'absolute', 'top': top + scrollTop, left: left + scrollLeft });
     }
-    else{
-        $("#num").val(num);
-    }
-})
-$("#add_num").click(function(){
-    var num = parseInt($("#num").val())+1;
-    $("#num").val(num);
-})
 
-function sleep(n){
-        var start=new Date().getTime();
-        while(true){
-            if(new Date().getTime()-start > n)
-            {
-                break;
+
+/*列表页动态显示*/
+function show_goods(){
+	var goods_container = $('.content-container');//content最外层div
+	goods_container.html('');//清空div
+
+	var item_url = '';//商品接口
+
+	$.ajax({
+		type:"GET",
+		url:item_url,
+		dataType:"json",
+		beforeSend: function() {
+            NProgress.start();
+        },
+        complete: function() {
+            NProgress.done();
+        },
+        success:function(msg){
+        	if(msg.status==1){
+        		var goods = msg.value;
+                var html = '';
+                html += '<h1 class="cover-heading"><span class="hint--right" aria-label="Notice!">All</span></h1>';
+                html += '<div class="lead">';
+                html += '<ul class="rules">';
+                html += '</ul>';
+                html += '</div>'
+                goods_container.html(html);
+                flush_data();
+                var ul_list = $(".rules");//列表ul
+                var jsonObj=JSON.parse(goods);//商品列表json数据
+                //加载列表
+                for(var i=0;i<jsonObj.length;i++){
+                    var id = jsonObj[i].item_id;
+                    var li = $("<li>").addClass("goods").attr("id",id).appendTo(ul_list);
+                    var img = $("<img>").attr("src",jsonObj[i].img).appendTo(li);
+                    var name = $("<h1>").text(jsonObj[i].name).appendTo(li);
+                    var price = $("<p>").text(jsonObj[i].price).appendTo(li);
+                }
+                //获取商品详情
+                $(".goods").on("click",function(){
+                	var id = $(this).attr("id");//获取id
+                	for(var i=0;i<jsonObj.length;i++){
+                		if(id == jsonObj[i].item_id){
+	                		//创建详情弹窗
+		                	var dialog = $("<div>").addClass("dialog").appendTo(goods_container);
+		                	var dialog_content = $("<div>").addClass("dialog_content").appendTo(dialog);
+		                	var dialog_close = $("<button>").attr("id","dialog_close").text("×").appendTo(dialog_content);
+		                	var dialog_img = $("<img>").attr("src",jsonObj[i].img).appendTo(dialog_content);
+		                	var dialog_text = $("<div>").addClass("dialog_text").appendTo(dialog_content);
+		                	var text_h1 = $("<h1>").text(jsonObj[i].name).appendTo(dialog_text);
+		                	var text_p = $("<p>").text('￥' + jsonObj[i].price).appendTo(dialog_text);
+		                	var sub = $("<button>").addClass("num").attr("id","sub_num").text("-").appendTo(dialog_text);
+		                	var input = $("<input>").attr("type","text","id","num","value","1").appendTo(dialog_text);
+		                	var add = $("<button>").addClass("num").attr("id","add_num").text("＋").appendTo(dialog_text);
+		                	var add_cart = $("<button>").attr("id","add_cart").text("ADD").appendTo(dialog_text);
+		                	var description = $("<p>").addClass("description").text(jsonObj[i].description).appendTo(dialog_content);
+	                	}
+                	}
+                	//详情弹窗显示
+                	$(".dialog").css({"display":"block"});
+                	funShowDivCenter(".dialog_content");
+                })
+                //数量加减
+				$("#sub_num").click(function(){
+				    var num = parseInt($("#num").val())-1;
+				    if(num<0){
+				        $("#num").val(0);
+				    }
+				    else{
+				        $("#num").val(num);
+				    }
+				})
+				$("#add_num").click(function(){
+				    var num = parseInt($("#num").val())+1;
+				    $("#num").val(num);
+				})
+
+				//关闭弹框
+				$("#dialog_close").click(function(){
+				    $(".dialog").css({"display":"none"});
+				})
+        	} else {
+                show_pnotify("Failed!", msg.message, "error");
             }
         }
-    }//function sleep(n)
+	})
+}
+
+
+
+show_goods();
+
+
+
+/*购物车部分*/
+
+function sleep(n){
+    var start=new Date().getTime();
+    while(true){
+        if(new Date().getTime()-start > n)
+        {
+            break;
+        }
+    }
+}//function sleep(n)
 
 
 //封装该操作，以便后续直接调用.
- function GetGoodsInfoByIds(Num){//根据购物车返回的各商品id，再次向后台要对应各id的商品的具体信息.
+function GetGoodsInfoByIds(Num){//根据购物车返回的各商品id，再次向后台要对应各id的商品的具体信息.
     $.ajax({
     type:"POST",
-   // data:"ids[]",
+    // data:"ids[]",
     url:"/assets/js/cartGoodsInfo.json",
     dataType:"json",
     success:function(GoodsInfo){//msg
         console.log(111);
         //if(msg.status==1)//{
-       // var cartInfo=json.parse(msg.value);
-       $(".cart-tbody").html('');
-       for(i=0;i<GoodsInfo.length;i++)
+        // var cartInfo=json.parse(msg.value);
+        $(".cart-tbody").html('');
+        for(i=0;i<GoodsInfo.length;i++)
         {  // console.log(i);
             var name=GoodsInfo[i].Name;
             var price=GoodsInfo[i].Price;
@@ -95,109 +183,6 @@ $(document).ready(function(){
 })
 })
 
-// //添加到购物车(静态注释版)
-// $("#add_cart").click(function(){
-//     var name = $(".dialog_text").find("h1").text();
-//     var price = $(".dialog_text").find("p").text();
-//     var num = parseInt($("#num").val());
-//     var table = $(".cart-tbody");
-//     var table_data = $("<tr>").appendTo(table);
-//     var table_name = $("<td>").text(name).appendTo(table_data);
-//     var table_price = $("<td>").text(price).appendTo(table_data);
-//     var table_num = $("<td>").text(num).appendTo(table_data);
-//     $(".dialog").css({"display":"none"});
-// })
-
-
-//列表交互
-$(document).ready(function(){
-    var ul_list = $(".rules");
-    var i;
-    appendList();
-    function appendList(){
-        console.log(222);
-        var url = "/assets/js/data.json";
-        $.ajax({
-            type:"GET",
-            url:url,
-            dataType:"json",
-            success:function(jsonObj){
-                console.log(111);
-                //var jsonObj=JSON.parse(msg);
-                console.log(jsonObj);
-                for(i=0;i<jsonObj.length;i++){
-                    var id = jsonObj[i].item_id;
-                    var li = $("<li>").addClass("goods").attr("id",id).appendTo(ul_list);
-                    var img = $("<img>").attr("src",jsonObj[i].img).appendTo(li);
-                    var name = $("<h1>").text(jsonObj[i].name).appendTo(li);
-                    var price = $("<p>").text(jsonObj[i].price).appendTo(li);
-                }
-                //点击获取详情
-                $(".goods").on("click",function(){
-                    var id = $(this).attr("id");
-                    var dialog_content = $(".dialog_content");
-                    var dialog_text = $(".dialog_text");
-                    console.log(id);
-                    for(i=0;i<jsonObj.length;i++){
-                        if(id == jsonObj[i].item_id){
-                            dialog_content.find("img").attr("src",jsonObj[i].img);
-                            dialog_text.find("h1").text(jsonObj[i].name);
-                            dialog_text.find("p").text(jsonObj[i].price);
-                            $(".description").text(jsonObj[i].description);
-                        }
-                    }
-                    $(".dialog").css({"display":"block"});
-                    funShowDivCenter(".dialog_content");
-                })
-
-            }
-        })
-    }
-    //点击获取详情
-    // $(".goods").on("click",function(){
-    // 	var url = "/assets/js/data.json";
-    // 	var id = $(this).attr("id");
-    // 	var dialog_content = $(".dialog_content");
-    // 	var dialog_text = $(".dialog_text");
-    // 	console.log(id);
-    // 	$.ajax({
-    // 		type:"GET",
-    // 		url:url,
-    // 		// data:{item_id:id},
-    // 		dataType:"json",
-    // 		success:function(jsonObj){
-    // 			console.log(111);
-    // 			//var jsonObj=JSON.parse(msg);
-    // 			console.log(jsonObj);
-    // 			dialog_content.find("img").attr("src",jsonObj.img);
-    // 			dialog_text.find("h1").text(jsonObj.name);
-    // 			dialog_text.find("p").text(jsonObj.price);
-    // 			$(".description").text(jsonObj.description);
-
-    // 		}
-    // 	})
-    // 	$(".dialog").css({"display":"block"});
-    // 	funShowDivCenter(".dialog_content");
-
-    // })
-    function funShowDivCenter(div) {
-        var top = ($(window).height(0
-        ) - $(div).height()) / 2;
-        var left = ($(window).width() - $(div).width()) / 2;
-        var scrollTop = $(document).scrollTop();
-        var scrollLeft = $(document).scrollLeft();
-        $(div).css({ display:'block',position: 'absolute', 'top': top + scrollTop, left: left + scrollLeft });
-    }
-})
-
-
-//关闭弹框
-$("#dialog_close").click(function(){
-    $(".dialog").css({"display":"none"});
-})
-
-
-
 //购物车更新(用户登录后，自动显示其购物车中的信息)
 $(document).ready(function(){
 
@@ -227,3 +212,69 @@ $(document).ready(function(){
     sleep(200);
     GetGoodsInfoByIds(Numbers);
 })
+
+
+//添加到购物车
+// $("#add_cart").click(function(){
+//     var name = $(".dialog_text").find("h1").text();
+//     var price = $(".dialog_text").find("p").text();
+//     var num = parseInt($("#num").val());
+//     var table = $(".cart-tbody");
+//     var table_data = $("<tr>").appendTo(table);
+//     var table_name = $("<td>").text(name).appendTo(table_data);
+//     var table_price = $("<td>").text(price).appendTo(table_data);
+//     var table_num = $("<td>").text(num).appendTo(table_data);
+//     $(".dialog").css({"display":"none"});
+// })
+
+
+//列表交互
+// $(document).ready(function(){
+//     var ul_list = $(".rules");
+//     var i;
+//     appendList();
+//     function appendList(){
+//         console.log(222);
+//         var url = "/assets/js/data.json";
+//         $.ajax({
+//             type:"GET",
+//             url:url,
+//             dataType:"json",
+//             success:function(jsonObj){
+//                 console.log(111);
+//                 //var jsonObj=JSON.parse(msg);
+//                 console.log(jsonObj);
+//                 for(i=0;i<jsonObj.length;i++){
+//                     var id = jsonObj[i].item_id;
+//                     var li = $("<li>").addClass("goods").attr("id",id).appendTo(ul_list);
+//                     var img = $("<img>").attr("src",jsonObj[i].img).appendTo(li);
+//                     var name = $("<h1>").text(jsonObj[i].name).appendTo(li);
+//                     var price = $("<p>").text(jsonObj[i].price).appendTo(li);
+//                 }
+//                 //点击获取详情
+//                 $(".goods").on("click",function(){
+//                     var id = $(this).attr("id");
+//                     var dialog_content = $(".dialog_content");
+//                     var dialog_text = $(".dialog_text");
+//                     console.log(id);
+//                     for(i=0;i<jsonObj.length;i++){
+//                         if(id == jsonObj[i].item_id){
+//                             dialog_content.find("img").attr("src",jsonObj[i].img);
+//                             dialog_text.find("h1").text(jsonObj[i].name);
+//                             dialog_text.find("p").text(jsonObj[i].price);
+//                             $(".description").text(jsonObj[i].description);
+//                         }
+//                     }
+//                     $(".dialog").css({"display":"block"});
+//                     funShowDivCenter(".dialog_content");
+//                 })
+
+//             }
+//         })
+//     }
+    
+// })
+
+
+
+
